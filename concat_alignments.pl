33d3986c9f28e5733a9885722c922d329f85550d
vinuesa <vinuesa@ccg.unam.mx>
2017-05-13 00:34:39 -0500

    stripped down version; does not require phyTools.pm

--- concat_alignments.pl
+++ concat_alignments.pl
@@ -1,13 +1,14 @@
-#!/usr/bin/perl -w
+#!/usr/bin/env perl
 # Pablo Vinuesa, Bruno Contreras Moreira
 # 2007 UNAM, Mexico
 
 use strict;
+use warnings;
 use File::Basename;
 
 my $progname = basename($0);
 my $REMOVETMPFILES = 1;
-my $VERSION = 1.0 # May 13th, 2017. Stripped down version, that does not depend on phyTools
+my $VERSION = 1.0; # May 13th, 2017. Stripped down version, that does not depend on phyTools
 
 ######################################################
 
@@ -25,7 +26,7 @@ else{ ($listfile) = @ARGV;  }
 
 if(! -s $listfile){ die "#$progname : need a valid list file\n"; }
 
-print "# $progname params: input file $listfile\n";
+#print "# $progname params: input file $listfile\n";
 
 ######################################################
 
@@ -47,7 +48,7 @@ foreach $file (@aln_files)
 
     foreach $seq (keys(%FASTAaln))
     {
-    	    if(!$concat_aln{$seq}{'LENGTH'})	    { $concat_aln{$seq}{'LENGTH'} = 0; }
+    	    if(!$concat_aln{$seq}{'LENGTH'}){ $concat_aln{$seq}{'LENGTH'} = 0; }
 
     	    # concat headers ommited in concat_aln_local.pl 
     	    #$concat_aln{$seq}{'NAME'} .= $FASTAaln{$seq}{'NAME'};
@@ -69,7 +70,10 @@ foreach $file (@aln_files)
 }	
 
 # 3) print concatenated alignment with full names to STDOUT
-print "# concatenation coordinates:\n$concat_aln{1}{'COORDS'}\n"; 
+my $coords_file="concatenation_coordinates.txt";
+open COORDS, ">", "$coords_file" or die "can't write to $coords_file: $!\n\n";
+print COORDS "# concatenation coordinates:\n$concat_aln{1}{'COORDS'}\n"; 
+close COORDS;
 
 foreach $seq (sort {$a<=>$b} (keys(%concat_aln)))  # numeric sort of seq IDs
 {
