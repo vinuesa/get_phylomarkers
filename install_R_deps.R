be8477fffb90ca226fe419b1019803d9aef3b395
eead-csic-compbio <bcontreras@eead.csic.es>
2017-05-15 18:41:58 +0200

    added instructions to solve common issues

--- install_R_deps.R
+++ install_R_deps.R
@@ -2,8 +2,22 @@
 
 # checks installed R packages and installs only the missing ones 
 
-repository = 'http://cran.rstudio.com/'; #'https://cloud.r-project.org'
+# Instructions to update R on Ubuntu systems, Xenial in the example:
 
-required_packages = c("ape", "kdetrees", "stingr", "vioplot", "ggplot2", "gplots", "plyr", "seqinr")
+# $ sudo echo "deb http://cran.rstudio.com/bin/linux/ubuntu xenial/" | sudo tee -a /etc/apt/sources.list
+# gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E084DAB9
+# gpg -a --export E084DAB9 | sudo apt-key add -
+# $ sudo apt-get update
+# $ sudo apt-get install r-base r-base-dev
+
+# Instructions in case some of the extant packages are not up-to-date:
+
+# $ R
+# > remove.packages("Rcpp")
+# > install.packages("Rcpp",dependencies=TRUE, lib="lib/R", repos="https://cloud.r-project.org")
+
+repository = 'https://cloud.r-project.org'; #'http://cran.rstudio.com/';
+
+required_packages = c("ape", "kdetrees", "stringr", "vioplot", "ggplot2", "gplots", "plyr", "seqinr")
 new.packages = required_packages[!(required_packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages, dependencies=TRUE, lib="lib/R", repos=repository)
