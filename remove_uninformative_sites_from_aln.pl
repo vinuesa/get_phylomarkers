9b71965c862cc01430a99b418f61966a3554857a
eead-csic-compbio <bcontreras@eead.csic.es>
2017-05-16 13:01:04 +0200

    added use lib to fetch local bioperl1.5.2

--- remove_uninformative_sites_from_aln.pl
+++ remove_uninformative_sites_from_aln.pl
@@ -8,6 +8,11 @@ use strict;
 use warnings;
 use Data::Dumper;
 use Getopt::Long;
+
+use FindBin '$Bin'; # added by Bruno May2017
+use lib "$Bin/lib";
+use lib "$Bin/lib/perl/bioperl-1.5.2_102/";
+
 use Bio::SeqIO;
 
 sub logmsg{$|++;print STDERR "@_\n"; $|--;}
