ffe96c9b22e803bad0c8b4bd2f46eacad806501f
vinuesa <vinuesa@ccg.unam.mx>
2021-09-15 11:59:53 -0500

    install.packages(..., clean = TRUE)

--- install_R_deps.R
+++ install_R_deps.R
@@ -40,7 +40,7 @@ local_lib = "./lib/R"
 for (package in required_packages) {
   if (!require(package, character.only=T, quietly=T)) {
     sprintf("# cannot load %s, will get it from %s and install it in %s",package,repository,local_lib)
-    install.packages(package, dependencies=TRUE, lib=local_lib, repos=repository)
+    install.packages(package, dependencies=TRUE, lib=local_lib, repos=repository, clean = TRUE)
   }
 }
 
