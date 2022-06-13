ae603e8372c7805b7a54163066fe4d98716ae2ae
vinuesa <vinuesa@ccg.unam.mx>
2022-06-12 23:52:17 -0500

    first commit

---
+++ install_kdetrees_from_src.R
@@ -0,0 +1,9 @@
+#!/usr/bin/env Rscript
+
+# Pablo Vinuesa
+# 2022-06-12
+
+# install /kdetrees_0.1.2.tar.gz from source, as it was removed from CRAN!
+install.packages(src/kdetrees_0.1.2.tar.gz, repos = NULL, type="source")
+
+sessionInfo()
