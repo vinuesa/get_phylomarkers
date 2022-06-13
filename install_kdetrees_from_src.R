faa53a0af85064482e10185b1cfa1f0162e57485
vinuesa <vinuesa@ccg.unam.mx>
2022-06-13 18:12:35 -0500

    removed install_kdetrees_from_src.R

--- install_kdetrees_from_src.R
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env Rscript
-
-# Pablo Vinuesa
-# 2022-06-12
-
-# install /kdetrees_0.1.2.tar.gz from source, as it was removed from CRAN!
-install.packages("lib/R/kdetrees_0.1.2.tar.gz", repos = NULL, type="source")
-
-sessionInfo()
