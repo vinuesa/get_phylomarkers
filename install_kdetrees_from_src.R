b0920d2a406b1cf50f795cc4f37968c3144ae333
vinuesa <vinuesa@ccg.unam.mx>
2022-06-13 00:37:33 -0500

    installs kdetrees from src lib/R/kdetrees_0.1.2.tar.gz

--- install_kdetrees_from_src.R
+++ install_kdetrees_from_src.R
@@ -4,6 +4,6 @@
 # 2022-06-12
 
 # install /kdetrees_0.1.2.tar.gz from source, as it was removed from CRAN!
-install.packages("src/kdetrees_0.1.2.tar.gz", repos = NULL, type="source")
+install.packages("lib/R/kdetrees_0.1.2.tar.gz", repos = NULL, type="source")
 
 sessionInfo()
