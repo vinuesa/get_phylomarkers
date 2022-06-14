d59d77df0e71ddf83d1b756c3865aa7c4f0593bf
vinuesa <vinuesa@ccg.unam.mx>
2022-06-13 19:48:32 -0500

    first commit; called Dockerfile to install kdetrees from its github repo

---
+++ install_kdetrees_from_github.R
@@ -0,0 +1,30 @@
+#!/usr/bin/env Rscript
+
+# 2022-06-13
+# install_kdetrees_from_github
+# call to build docker image
+
+# install /kdetrees_0.1.5.tar.gz from GitHub, as it was removed from CRAN!  
+#   > Archived on 2022-05-10 as email to the maintainer is undeliverable. 
+# https://cran.r-project.org/src/contrib/Archive/kdetrees/ << version 
+# PV 2022-06-12s; GitHub repo 'http://github.com/grady/kdetrees'
+# install.packages("kdetrees_0.1.2.tar.gz", lib='http://github.com/grady/kdetrees'  repos = NULL, type="source")
+
+
+required_packages <- c("devtools")
+
+
+# install the remaining required R packages
+for (package in required_packages) {
+  if (!require(package, character.only=T, quietly=T)) {
+    sprintf("# cannot load %s, will get it from %s and install it in %s",package,repository,local_lib)
+    install.packages(package, dependencies=TRUE, clean=TRUE)
+  }
+}
+
+
+library("devtools")
+install_github("grady/kdetrees", dependencies = TRUE)
+
+
+sessionInfo()
