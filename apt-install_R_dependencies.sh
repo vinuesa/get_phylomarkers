6a8d040a15d2c18f04ba403f931a2595dc1e149c
vinuesa <vinuesa@ccg.unam.mx>
2022-07-02 11:12:10 -0500

    v2022-07-02; added sudo and --yes

--- apt-install_R_dependencies.sh
+++ apt-install_R_dependencies.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-# 2022-06-13
+# 2022-07-02 # sudo and add-apt-repository --yes
 # Install R-packages on ubuntu with apt install
 
 wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
-add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
-add-apt-repository ppa:c2d4u.team/c2d4u4.0+
+sudo add-apt-repository --yes "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+sudo add-apt-repository --yes ppa:c2d4u.team/c2d4u4.0+
 
 
-apt install --no-install-recommends -y \
+sudo apt install --no-install-recommends -y \
 r-cran-ape \
 r-cran-phangorn \
 r-cran-devtools \
@@ -21,4 +21,4 @@ r-cran-ggplot2 \
 r-cran-stringi \
 r-cran-stringr \
 r-cran-seqinr \
-&& apt clean && apt purge && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+&& sudo apt clean && apt purge && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
