50bfbd77e60621be55061569827278b4d653d108
vinuesa <vinuesa@ccg.unam.mx>
2024-04-15 19:21:11 -0600

    apt-install_R_dependencies.sh v2024-04-15: removed unnecessary packages

--- apt-install_R_dependencies.sh
+++ apt-install_R_dependencies.sh
@@ -3,27 +3,23 @@
 # Author: Pablo Vinuesa, CCG-UNAM; @pvinmex
 # 2022-07-02 # sudo and add-apt-repository --yes
 # Install R-packages on ubuntu with apt install
+# version 2024-10-15
 
 wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
 sudo add-apt-repository --yes "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
-sudo add-apt-repository --yes ppa:c2d4u.team/c2d4u4.0+
-
+#sudo add-apt-repository --yes ppa:c2d4u.team/c2d4u4.0+
 
 sudo apt install --no-install-recommends -y \
 r-cran-ape \
-r-cran-phangorn \
-r-cran-devtools \
-r-cran-cluster \
+r-cran-remotes \
 r-cran-gplots \
 r-cran-vioplot \
 r-cran-plyr \
 r-cran-dplyr \
 r-cran-ggplot2 \
-r-cran-stringi \
 r-cran-stringr \
 r-cran-seqinr \
-r-cran-remotes \
 && sudo apt clean && apt purge && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 
-#NOTE: may need to install stringi as roow with install.packages("stringi", dep=T)
+#NOTE: may need to install stringi as root with install.packages("stringi", dep=T)
